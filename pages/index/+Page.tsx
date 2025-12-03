8d80015c92430994283ba0a3dd80758f9ae003ca
Quentin Derimais <quentin.derimais@gmail.com>
2025-12-03 14:04:32 +0100

    Style : retrait de la propriété fixed au header

--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -32,25 +32,9 @@ export default function Home() {
 
     return (
         <>
-        <div class=''>
-            <Heading component="h1" level={1} bold="bold">Coucou le monde</Heading>
-            <Heading component="h2" level={2} bold="bold">Coucou le monde</Heading>
-            <Heading component="h3" level={3} bold="bold">Coucou le monde</Heading>
-            <Heading component="h4" level={4} bold="bold">Coucou le monde</Heading>
-            <Heading component="h5" level={5} bold="bold">Coucou le monde</Heading>
-            <Heading component="h6" level={6} bold="bold">Coucou le monde</Heading>
-        </div>
-        <div>
-            <Text component="p" level={1}>Text niveau 1</Text>
-            <Text component="p" level={2}>Text niveau 2</Text>
-            <Text component="p" level={3}>Text niveau 3</Text>
-            <Text component="p" level={4}>Text niveau 4</Text>
-            <Text component="p" level={5}>Text niveau 5</Text>
-        </div>
-
-        <div>
-            <Project project={project} />
-        </div>
+            <div class='flex flex-col'>
+                <Text class="align-center text-align-center">Lorem ipsum</Text>
+            </div>
         </>
     )
 }
