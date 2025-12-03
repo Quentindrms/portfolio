d1c384644ed868e0476ce405c829c0c5e9e6a471
Quentin Derimais <quentin.derimais@gmail.com>
2025-12-03 10:00:30 +0100

    Style : Création du composant heading

---
+++ components/heading.tsx
@@ -0,0 +1,52 @@
+import { splitProps, type JSX } from "solid-js";
+import { Dynamic } from "solid-js/web";
+import {clsx} from "clsx";
+
+type HeadingLevel = 1 | 2 | 3 | 4 | 5 | 6;
+type BoldValue = "light" | "regular" | "bold" | "extra-bold" | "black";
+
+export interface HeadingProps extends JSX.HTMLAttributes<HTMLHeadingElement> {
+    component?: `h${HeadingLevel}`
+    children: JSX.Element,
+    bold?: BoldValue;
+    level?: HeadingLevel,
+}
+
+const globalClasses = "text-primary-text"
+
+const levelclasses: Record<HeadingLevel, string> = {
+    1: 'text-6xl',
+    2: 'text-5xl',
+    3: 'text-4xl',
+    4: 'text-3xl',
+    5: 'text-2xl',
+    6: 'text-xl'
+}
+
+const boldCLasses: Record<BoldValue, string> = {
+    light : "font-light",
+    regular : "font-regular",
+    bold: "font-bold",
+    "extra-bold": "font-extrabold",
+    black: "font-black",
+}
+
+function getLevelClasses(props: HeadingLevel|undefined){
+    return levelclasses[props ?? 1];
+}
+
+function getBoldCLasses(props: BoldValue|undefined){
+    return boldCLasses[props ?? "regular"];
+}
+
+export function Heading(props:HeadingProps) {
+
+    const [local, rest] = splitProps(props, ["level", "component", "bold"]);
+    const Component = local.component || `h${local.level ?? 1}`; 
+
+    return (
+        <>
+        <Dynamic {...rest} component={Component} class={clsx([globalClasses, getLevelClasses(props.level), getBoldCLasses(props.bold)])}></Dynamic>
+        </>
+    )
+}
--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -1,9 +1,15 @@
+import { Heading } from "../../components/heading";
 
 export default function Home() {
 
     return (
         <div class=''>
-            <h1 class='text-stone-50 font-title-primary'>Bonjour le monde !</h1>
+            <Heading component="h1" level={1} bold="bold">Coucou le monde</Heading>
+            <Heading component="h2" level={2} bold="bold">Coucou le monde</Heading>
+            <Heading component="h3" level={3} bold="bold">Coucou le monde</Heading>
+            <Heading component="h4" level={4} bold="bold">Coucou le monde</Heading>
+            <Heading component="h5" level={5} bold="bold">Coucou le monde</Heading>
+            <Heading component="h6" level={6} bold="bold">Coucou le monde</Heading>
         </div>
     )
 }
