a1a7191e96450f58aab94b107e64828f352f4ee5
Quentin Derimais <quentin.derimais@gmail.com>
2025-12-07 11:27:58 +0100

    Fix : corrections des url pour import des images

--- data/data.ts
+++ data/data.ts
@@ -4,27 +4,33 @@ import { ContactType, ProjectType } from "../types/Types"
 export const hardSkills = ["Typescript", "React", "SolidJS", "PostgreSQL", "Docker"];
 export const softSkills = ["Adaptabilité", "Rigueur", "Curiosité", "Autonomie", "Esprit d'analyse et de synthèse"];
 
+import dinoPark from "../assets/images/screenshots/dinopark.png";
+import fluxFlims from "../assets/images/screenshots/flux-films.png";
+import spaceInvader from "../assets/images/screenshots/space-invader.png"
+import githubLogo from "../assets/images/illustrations/github.svg"
+import linkedinLogo from "../assets/images/illustrations/linkedin.svg"
+
 export const project: ProjectType[] = [
     {
         nom: "Space Invaders",
         description: "Un ramake du celebre jeu Space Invaders, réalisé durant ma formation",
         technologies: ["Typescript"],
         github: "#",
-        image: "../assets/images/screenshots/space-invader.png"
+        image: spaceInvader
     },
     {
         nom: "Dino Park",
         description: "Un parc à thème dinosaures, il est possible de gérer le parc et de passer commande.",
         technologies: ["Typescript", "PostgreSQL"],
         github: "#",
-        image: "../assets/images/screenshots/dinopark.png"
+        image: dinoPark
     },
     {
         nom: "Flux Films",
         description: "Une plateforme de streaming avec les données API de The Movie Database.",
         technologies: ["Typescript", "React"],
         github: "#",
-        image: "../assets/images/screenshots/flux-films.png"
+        image: fluxFlims
     }
 ];
 
@@ -32,13 +38,13 @@ export const contacts: ContactType[] = [
     {
         name: "Github",
         alt: "Image cliquable menant à mon profil Github",
-        image: "../assets/images/illustrations/github.svg",
+        image: githubLogo,
         lien: 'https://github.com/Quentindrms'
     },
         {
         name: "Linkedin",
         alt: "Image cliquable menant à mon profil Linkedin",
-        image: "../assets/images/illustrations/linkedin.svg",
+        image: linkedinLogo,
         lien: 'https://www.linkedin.com/in/quentin-derimais/',
     }
 ]
